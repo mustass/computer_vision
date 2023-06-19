3431c606152525796c67a894d657c8e382d482e1
Stefan Peter Engelmann Jensen <stefanengelmann10@hotmail.com>
2023-06-19 10:28:43 +0200

    updated segmentation_utils.py

--- src/dl4cv/utils/segmentation_utils.py
+++ src/dl4cv/utils/segmentation_utils.py
@@ -13,20 +13,20 @@ def plot_results(preds, labels, images, epoch, batch_index , batch_size, dir_pat
 
     # Define ImageNet mean and std
   
-    plt.rcParams["figure.figsize"] = [6, 6]
+    plt.rcParams["figure.figsize"] = [18, 18]
     preds = sigmoid(preds) > 0.5
     f, axarr = plt.subplots(3,batch_size)
     for k in range(batch_size):    
         img = images[k].cpu().numpy()
         img = np.rollaxis(img, 0, 3)
         axarr[0,k].imshow(img)
-        axarr[0,k].title.set_text("Real")
+        axarr[0,k].set_title("Real",fontsize=28)
         axarr[0,k].axis("off")
         axarr[1,k].imshow(preds[k].cpu().numpy(), cmap="gray")
-        axarr[1,k].title.set_text("Output")
+        axarr[1,k].set_title("Output",fontsize=28)
         axarr[1,k].axis("off")
         axarr[2,k].imshow(labels[k].cpu().numpy(), cmap="gray")
-        axarr[2,k].title.set_text("True")
+        axarr[2,k].set_title("True",fontsize=28)
         axarr[2,k].axis("off")
     
 
@@ -34,7 +34,7 @@ def plot_results(preds, labels, images, epoch, batch_index , batch_size, dir_pat
 
     if not os.path.exists(dir_path + "/plots"):
         os.makedirs(dir_path + "/plots")
-    plt.savefig(dir_path + f"/plots/plots_{string}.png")
+    plt.savefig(dir_path + f"/plots/plots_{string}.png",transparent=True,bbox_inches='tight')
     wandb_logger.log_image(key='plots', images = [dir_path + f"/plots/plots_{string}.png"],caption = [string])
     os.remove(dir_path + f"/plots/plots_{string}.png")
 
